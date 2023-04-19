ac3a759107c2ecca548b267d7a515b58431c36fa
aj2000 <hi@ajaysharma.dev>
2023-04-19 21:18:07 +0530

    read me update

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+function aveSpd(upTime, upSpd, downSpd) {
+  // your code here
+}
+
+// Do not change the code below
+const upTime = prompt("Enter upTime: ");
+const downTime = prompt("Enter downTime: ");
+const downSpd = prompt("Enter downSpd: ");
+
+alert(aveSpd(upTime, downTime, downSpd));
